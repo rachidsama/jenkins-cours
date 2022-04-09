3939a0055132e8604c639d5c2a4cc523ec47be66
rachidsama <56230757+rachidsama@users.noreply.github.com>
2022-04-09 16:21:51 +0200

    Update Main.java
    activation du trigger

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
   public static void main (String [] args) {
-   System.out.println("Hello java world");
+   System.out.println("Hello java world, activation du trigger git version");
    }
  }
