a6f1222a92675f630bb8a93c2a8cd64d5942b79f
rachid <56230757+rachidsama@users.noreply.github.com>
2022-04-10 18:00:09 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
   public static void main (String [] args) {
    System.out.println("Activation du trigger git version");
+    int val = 2;
+    if(val>1){
+      System.out.println("Oui val sup à 2");
+    }
+    else{
+    System.out.println("Non val < à 2");
+    }
    }
  }
